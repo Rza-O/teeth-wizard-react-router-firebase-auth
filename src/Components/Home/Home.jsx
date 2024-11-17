5d1b3de99505821522b091a54fe93ab4ee9cbac2
appoclayptor <shahreza1414@gmail.com>
2024-11-17 13:09:29 +0600

    added dynamically fetched service cards

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import { useLoaderData } from 'react-router-dom';
+import ServiceCard from '../ServiceCard/ServiceCard';
 
 const Home = () => {
+    const services = useLoaderData();
+    console.log(services);
     return (
         <div>
-            <p className='text-red-700 text-5xl'>I am Home</p>
+            <Banner></Banner>
+            <div className='grid grid-cols-4 w-[80%] mx-auto max-w-[1200] gap-6 mt-7'>
+                {
+                    services.map(service => <ServiceCard service={service}></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ServiceCard = ({service}) => {
+    console.log(service);
+    const {treatment, image, description, cost} = service;
+    return (
+        <div className="card bg-base-100 shadow-xl">
+            <figure>
+                <img
+                    src={image}
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {treatment}
+                    <div className="badge badge-secondary">${cost}</div>
+                </h2>
+                <p title={description}>{description.slice(0,100)}...</p>
+                <div className="card-actions justify-end">
+                    <button className="badge badge-outline bg-primary text-white font-bold p-4">Check Out More</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
